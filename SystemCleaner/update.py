61d93251424cf74050265efa33c0d99deb470332
WAP Industries <141229412+WAP-Industries@users.noreply.github.com>
2023-12-12 11:59:40 +0800

    Update update.py

--- SystemCleaner/update.py
+++ SystemCleaner/update.py
@@ -6,12 +6,12 @@ with open(f"{dir}\\name.txt") as f:
     CurrentName = f.read().strip()
 
 def main():
-    link = input("Enter new malware name: ").strip()
+    NewName = input("Enter new malware name: ").strip()
     for path in [f"{dir}\\{i}" for i in os.listdir(dir) if os.path.basename(__file__) not in i and "exe" not in i]:
         with open(path) as f: 
             content = f.read()
         with open(path, "w") as f: 
-            f.write(content.replace(CurrentName, link))
+            f.write(content.replace(CurrentName, NewName))
             print(f"Successfully overwrote [{path}]")
 
 
